f5997638e78f3d99f7ad37b41606079b95c3e6bb
Ben Nicholson <58550277+bsncubed@users.noreply.github.com>
2023-01-09 21:48:02 +1100

    Delete sharepoint-backup.bash

--- sharepoint-backup.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-YL='\033[1;33m'
-now=$(date +"%Y%m%d")
-mkdir /tmp/wikibak
-tar -czvf "/tmp/wikibak/wikiname-$now.tar.gz" /var/www/html/dokuwiki
-rclone copy "/tmp/wikibak/wikiname-$now.tar.gz"  wiki2share:Backup
-printf "${YL} Done"
